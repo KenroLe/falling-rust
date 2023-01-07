a5cb7798fb18e41139913150ab408a3ffaf21a79
Kenro Leinvald <Kenro2002@gmail.com>
2023-01-07 11:42:03 +0200

    Created a default struct for occupied space

--- src/engine/game.rs
+++ src/engine/game.rs
@@ -1,12 +1,14 @@
 use minifb::{Window, MouseButton, MouseMode};
 
+use super::RUST_POINT;
+
 pub fn tick(world: &mut Vec<Vec<crate::engine::Point>>,window:&Window) {
     if window.get_mouse_down(MouseButton::Left) {
         let opt = window.get_mouse_pos(MouseMode::Discard);
         match opt {
             Some(pos) => {
                 let world_pos = window_to_world_pos(pos);
-                world[world_pos.x][world_pos.y] = crate::engine::Point{color:0xe36414,kinematic:false,occupied:true};
+                world[world_pos.x][world_pos.y] = RUST_POINT;
             },
             None => {}
         }
--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -4,9 +4,14 @@ pub mod game;
 pub struct Point {
     pub color:u32,
     pub kinematic:bool,
-    pub occupied:bool
+    pub occupied:bool,
+    pub entity:Entity
 }
-pub const EMPTY_POINT :Point = Point{color:0x000000,kinematic:false,occupied:false};
+#[derive(Clone,Copy)]
+pub struct Entity {
+
+}
+
 #[derive(Clone,Copy)]
 pub struct Vector2 {
     x:usize,
@@ -17,3 +22,6 @@ pub const ZEROVECTOR:Vector2= Vector2{
     x:0,
     y:0
 };
+pub const EMPTY_ENTITY :Entity = Entity{};
+pub const EMPTY_POINT :Point = Point{color:0x000000,kinematic:false,occupied:false, entity:EMPTY_ENTITY};
+pub const RUST_POINT :Point = Point{color:0xe36414,kinematic:false,occupied:true, entity:EMPTY_ENTITY};
